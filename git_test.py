59dce4f4f067558763866535e78634f62c0990ad
zhank <190867435@qq.com>
2024-01-25 19:16:02 +0800

    hot-fix commit 1

--- git_test.py
+++ git_test.py
@@ -2,6 +2,7 @@ def hello_python():
     print("Hello, Python!")
     print("This is a function in Python.")
     print("Let's learn more about functions.")
+    print("Bye, Python!")
 
 
 hello_python()
